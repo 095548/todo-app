b571a820f77f61609234f7f9402c8f24f0e52dda
Maru <longing2022@gmail.com>
2026-05-30 11:56:30 +0900

    test: テストを追加しH2でDB分離

--- src/test/java/com/example/todo_app/TaskServiceTest.java
+++ src/test/java/com/example/todo_app/TaskServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class TaskServiceTest {
@@ -37,5 +38,25 @@ class TaskServiceTest {
         Task result = taskService.completeTask(created.getId());
 
         assertThat(result.getStatus()).isEqualTo(TaskStatus.COMPLETED);
+    }
+
+    @Test
+    void タスクの期限を変更できる(){
+
+        Task created = taskService.createTask("期限変更", LocalDateTime.of(2027, 1, 1, 0, 0));
+
+        Task result = taskService.updateDeadline(created.getId(), LocalDateTime.of(2028, 2, 2, 0, 0));
+
+        assertThat(result.getDeadlineAt()).isEqualTo(LocalDateTime.of(2028, 2, 2, 0, 0));
+    }
+
+    @Test
+    void 存在しないIDで完了しようとすると例外が出る() {
+        // Arrange
+        Long Id = 9999L;
+
+        // Act & Assert
+        assertThatThrownBy(() -> taskService.completeTask(Id))
+                .isInstanceOf(TaskNotFoundException.class);
     }
 }
